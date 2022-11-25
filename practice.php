6019325ab93a4f5e830fdebac11337fa02a20c09
Chikatanaka <ume12.vmd38@gmail.com>
2022-11-25 12:52:20 +0000

    PHP課題05

--- practice.php
+++ practice.php
@@ -1,49 +1,43 @@
 <?php
 
-$a=3;
-$b=7;
+$name = "chika";
 
-echo $a+$b;?>
-
-<?php
-$array_month=["1月","2月","3月","4月","5月","6月","7月","8月","9月","10月","11月","12月"];
-
-echo $array_month[7];?>
-
-<?php
-
-$hallo='Hello';
-$name='chika';
-$world='World';
-
-echo $hallo.$name.$world;
+if ($name = "chika"){
+    echo '私の名前はちかです';
+}else{
+    echo 'あなたの名前ではありません';
+}
 ?>
 
 <?php
 
-$techboost = 'tech';
+$total = 0;
 
-$techboost .= 'boost';
+for ($i = 0;$i <= 10000; $i++)
+{
+    $total += $i;
+}
 
-echo $techboost;
+echo $total;
+
 ?>
 
 <?php
 
-$calendar_2018 = [
-  "January" => "1月",
-  "February" => "2月",
-  "March" => "3月",
-  "April" => "4月",
-  "May" => "5月",
-  "June" =>"6月",
-  "July" => "7月",
-  "August" => "8月",
-  "September" => "9月",
-  "October" => "10",
-  "November" => "11月",
-  "December" => "12月"
-  ];
+$fruits = array("paineapple","mandarinorange","persimmon","watermelon","banana");
 
-// 12月を表示する
-echo $calendar_2018["December"];
+foreach($fruits as $fruits){
+    echo $fruits;
+    echo "\n";
+}
+
+?>
+
+<?php
+/* for文の始めの値を定義する /
+$start = 1;
+/* for文の終わりの値を定義する */
+$end = 100;
+$start =1;
+for($i = $start; $i <= $end; $i++){if($i % 5 == 0){echo $i;echo "\n";}}
+?>
